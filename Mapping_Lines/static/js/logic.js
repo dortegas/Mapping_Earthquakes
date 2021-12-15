fcaa2bcd6293aa5901d5e6450cb4b43e802ee2ad
David OS <dortega.segura@gmail.com>
2021-12-14 18:28:13 -0600

    Mapping lines added

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -3,37 +3,10 @@ console.log("working");
 
 // Create the map object with a center and zoom level.
 //let map = L.map('mapid').setView([40.7, -94.5], 4);
-//let map = L.map('mapid').setView([34.0522, -118.2437], 4);
-//let map = L.map('mapid').setView([36.1733, -120.1794], 7);
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
-
-// Coordinates for each point to be used in the line.
-/*let line = [
-  [33.9416, -118.4085],
-  [37.6213, -122.3790],
-  [40.7899, -111.9791],
-  [47.4502, -122.3088]
-];*/
-
-let line = [
-  [37.6213, -122.3790], // San Francisco Airpoort
-  [19.4363, -99.0718],  // Mexico City Airport
-  [30.1976, -97.6663],  // Austin-Bergstrom International Airport (AUS)
-  [43.6775, -79.6248],  // Toronto Pearson International Airport (YYZ)
-  [40.6414, -73.77800]  // JFK Airport
-];
-
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-  color: "blue",
-  weight: 4,
-  opacity: 0.5,
-  dashArray: "2,10"
-}).addTo(map);
+let map = L.map('mapid').setView([34.0522, -118.2437], 4);
 
 // Get data from cities.js
-//let cityData = cities;
+let cityData = cities;
 
 // Loop through the cities array and create one marker for each city.
 /*cityData.forEach(function(city) {
@@ -41,7 +14,7 @@ L.polyline(line, {
   L.marker(city.location)
   .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
   .addTo(map);
-});
+});*/
 
 cityData.forEach(function(city) {
   console.log(city)
@@ -52,7 +25,7 @@ cityData.forEach(function(city) {
   })
   .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
   .addTo(map);
-});*/
+});
 
 // Create the map object with a center and zoom level.
 /*let map = L.map("mapid", {
@@ -88,23 +61,17 @@ L.circleMarker([34.0522, -118.2437], {
   radius: 300,
   color: "black",
   fillColor: '#ffffa1'
-}).addTo(map);
+}).addTo(map);*/
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+/*let streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}", {
 attribution: "Map data © <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery (c) <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     accessToken: API_KEY
 });*/
 
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-attribution: "Map data © <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery (c) <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    accessToken: API_KEY
-});
 
-/*let streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+let streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}", {
 attribution: "Map data © <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery (c) <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     accessToken: API_KEY
